6a9ecf7653a23e7e09b6157773822a33f12eef53
Faizal Vasaya <faizalvasaya@Faizals-Macbook.local>
2022-05-14 11:29:52 +0530

    feat: Controller for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/@:username')
+  getUserByUsername(@Param('username') username: string): string {
+    // TODO
+    return `details of username = ${username}`;
+  }
+
+  @Get('/:userid')
+  getUserByUserid(@Param('userid') userid: string): string {
+    // TODO
+    return `details of username = ${userid}`;
+  }
+
+  @Post('/')
+  createNewUser(@Body() createUser: any): string {
+    return `create users`;
+  }
+
+  @Patch('/:userid')
+  updateUserDetails(@Param('userid') userid: string): string {
+    return `Update user with id ${userid}`;
+  }
+
+  @Put('/:userid/follow')
+  followUser() {
+    return `following user`;
+  }
+
+  @Delete('/:userid/follow')
+  unfollowUser() {
+    return `Unfolow user`;
+  }
+
+  @Get('/:userid/followers')
+  getFollowersOfUser() {
+    return `Get followers for user`;
+  }
+
+  @Get('/:userid/followees')
+  getFolloweesOfUser() {
+    return `Get followees of user`;
+  }
+}
